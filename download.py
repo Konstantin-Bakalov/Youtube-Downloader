dcb380f73493a0d787fd8153e47a7ca5fd99a60d
airbus <ichbrauchemehrplatz@gmail.com>
2023-05-13 11:41:39 +0200

    added args to yt constructor to resolve problem with videos, marked as 18+

--- download.py
+++ download.py
@@ -3,8 +3,12 @@ from sys import argv
 
 link = argv[1]
 
+def on_progress(a, b, c):
+    pass
+
 try :
-    yt = YouTube(link)
+    # yt = YouTube(link)
+    yt = YouTube(link, on_progress_callback=on_progress, use_oauth=True, allow_oauth_cache=True)
 
     # video = yt.streams.get_by_resolution('720p')
     video = yt.streams.get_highest_resolution()
